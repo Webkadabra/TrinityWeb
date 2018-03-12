ed3529ebb6d4c5f17c1b82f61c291808d6019435
Qblolz <frostQb@mail.ru>
2018-03-12 17:23:40 +0300

    upload for frontend

---
+++ frontend/controllers/FileStorageController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+
+
+
+/**
+ * FileStorageController implements the CRUD actions for FileStorageItem model.
+ */
+class FileStorageController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => [
+                            'upload',
+                            'upload-delete',
+                        ],
+                        'allow' => true,
+                        'roles' => ['@']
+                    ],
+                ],
+            ],
+        ];
+    }
+
+
+    public function actions()
+    {
+        return [
+            'upload' => [
+                'class' => 'trntv\filekit\actions\UploadAction',
+                'deleteRoute' => 'upload-delete'
+            ],
+            'upload-delete' => [
+                'class' => 'trntv\filekit\actions\DeleteAction'
+            ],
+        ];
+    }
+}
