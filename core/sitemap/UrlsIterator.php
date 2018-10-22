b5ee6fd672b145ea42676c2057b75e6f356f49b8
qblolz <frostQb@mail.ru>
2018-10-22 16:50:13 +0300

    - add sitemap

---
+++ core/sitemap/UrlsIterator.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace core\sitemap;
+
+use yii\helpers\Url as YiiUrl;
+
+use Sitemaped\Element\Urlset\Url;
+
+use core\models\Article;
+use core\models\Page;
+
+class UrlsIterator extends \AppendIterator
+{
+    /**
+     * UrlsIterator constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->append($this->createPageLinksGenerator());
+        $this->append($this->createArticleLinksGenerator());
+    }
+
+    /**
+     * @return \Generator
+     */
+    protected function createPageLinksGenerator()
+    {
+        $models = Page::find()->published()->each(1000);
+        foreach ($models as $model) {
+            /** @var Page $model */
+            $url = new Url(
+                YiiUrl::to(['/page/view', 'slug' => $model->slug], true),
+                (new \DateTime())->setTimestamp($model->updated_at),
+                Url::CHANGEFREQ_MONTHLY
+            );
+            yield $url;
+        }
+    }
+
+    /**
+     * @return \Generator
+     */
+    protected function createArticleLinksGenerator(): \Generator
+    {
+        $models = Article::find()->published()->each(1000);
+        foreach ($models as $model) {
+            /** @var Article $model */
+            $url = new Url(
+                YiiUrl::to(['/article/view', 'slug' => $model->slug], true),
+                (new \DateTime())->setTimestamp($model->updated_at),
+                Url::CHANGEFREQ_WEEKLY
+            );
+            yield $url;
+        }
+    }
+}
